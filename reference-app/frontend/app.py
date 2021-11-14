839736f2a994e184f21046e8358d2fef61ff513e
unknown <richard.jarnen@gmail.com>
2021-11-14 22:09:10 +0800

    jaeger fix - frontend-app

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, request
+### for tracing
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
 app = Flask(__name__)
+
+# for tracing
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,},
+                        service_name="frontend-app")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
+###
+
 
 @app.route('/')
 def homepage():
