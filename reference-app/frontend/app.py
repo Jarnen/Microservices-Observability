41aebb0da502f45b71825e3ab9c0f99e30505171
unknown <richard.jarnen@gmail.com>
2021-11-19 11:09:38 +0800

    fix: frontend-app - prometheus metrics tagerts

--- reference-app/frontend/app.py
+++ reference-app/frontend/app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template, request
 from jaeger_client import Config
 from flask_opentracing import FlaskTracing
 
+# for metrics
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
 
+# for metrics
+metrics = PrometheusMetrics(app)
+# static information as metric
+metrics.info('app_info', 'Frontend-app info', version='1.3')
+##
 # for tracing
 config = Config(
     config={
@@ -22,6 +30,15 @@ tracing = FlaskTracing(jaeger_tracer, True, app)
 @app.route('/')
 def homepage():
     return render_template("main.html")
+
+
+# for metrics
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
 
 if __name__ == "__main__":
